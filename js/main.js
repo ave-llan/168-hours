ede21053a15b40b4344e77cf5b8178333321f91c
John Leszczynski <jrleszczynski@gmail.com>
2015-10-30 16:40:38 -0400

    first stub of app with default activities

--- js/main.js
+++ js/main.js
@@ -1,3 +1,79 @@
 require('angular')
 
-console.log('Hello world, from main.js!')
+
+var app = angular.module('app', []);
+
+app.controller('ScheduleListCtrl', function($scope) {
+  $scope.activities = [
+  {'name': 'sleep',
+   'minutes': 420,
+   'daysPerWeek': 7,
+   'category': 'sleep'},
+  {'name': 'eat breakfast',
+   'minutes': 45,
+   'daysPerWeek': 7,
+   'category': 'food'},
+  {'name': 'Rudy AM walk',
+   'minutes': 30,
+   'daysPerWeek': 7,
+   'category': 'pet'},
+  {'name': 'eat lunch',
+   'minutes': 30,
+   'daysPerWeek': 7,
+   'category': 'food'},
+  {'name': 'Rudy lunch walk',
+   'minutes': 60,
+   'daysPerWeek': 7,
+   'category': 'pet'},
+  {'name': 'cook/eat dinner',
+   'minutes': 75,
+   'daysPerWeek': 7,
+   'category': 'food'},
+  {'name': 'Rudy PM walk',
+   'minutes': 30,
+   'daysPerWeek': 7,
+   'category': 'pet'},
+  {'name': 'exercise',
+   'minutes': 45,
+   'daysPerWeek': 5,
+   'category': 'health'},
+  {'name': 'study math',
+   'minutes': 60,
+   'daysPerWeek': 4,
+   'category': 'study'},
+  {'name': 'read novel',
+   'minutes': 45,
+   'daysPerWeek': 7,
+   'category': 'leisure'},
+  {'name': 'birdwatching',
+   'minutes': 150,
+   'daysPerWeek': 1,
+   'category': 'leisure'},
+  {'name': 'grocery shopping',
+   'minutes': 90,
+   'daysPerWeek': 1,
+   'category': 'chores'},
+  {'name': 'vacuum',
+   'minutes': 45,
+   'daysPerWeek': 1,
+   'category': 'chores'},
+  {'name': 'watch a movie',
+   'minutes': 120,
+   'daysPerWeek': 1,
+   'category': 'leisure'}
+  ]
+
+  $scope.timeRemaining = calculateTimeRemaining()
+
+  function calculateTimeRemaining () {
+    var timeUsed = $scope.activities.map(function (activity) {
+      return activity.minutes * activity.daysPerWeek
+    }).reduce(function (sum, time) {
+      return sum + time
+    })
+    return 60 * 168 - timeUsed // in minutes
+  }
+
+})
+
+
