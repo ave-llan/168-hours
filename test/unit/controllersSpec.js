81173cc844c6469c37896ce26e489050a02c1bd9
John Leszczynski <jrleszczynski@gmail.com>
2015-11-03 10:51:39 -0500

    revert to default empty tests

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,18 +1,11 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
-describe('PhoneCat controllers', function() {
 
-  describe('PhoneListCtrl', function(){
+describe('controllers', function() {
 
-    beforeEach(module('phonecatApp'));
-
-    it('should create "phones" model with 3 phones', inject(function($controller) {
-      var scope = {},
-          ctrl = $controller('PhoneListCtrl', {$scope:scope});
-
-      expect(scope.phones.length).toBe(3);
-    }));
+  it("should do something", function() {
 
   });
+
 });
