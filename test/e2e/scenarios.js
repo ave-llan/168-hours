09abe03d0b474437704d597ac6e3e58736175d9c
John Leszczynski <jrleszczynski@gmail.com>
2015-11-03 11:56:08 -0500

    add simple protractor test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,10 +2,28 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('168 Hours App', function() {
 
-  beforeEach(function() {
-    browser.get('app/index.html');
+  describe('Activity list view', function() {
+
+    beforeEach(function() {
+      browser.get('app/index.html');
+    });
+
+
+    it('should filter the activty list as a user types into the search box', function() {
+
+      var activityList = element.all(by.repeater('activity in activities'));
+      var query = element(by.model('query'));
+
+      expect(activityList.count()).toBe(16);
+
+      query.sendKeys('rudy');
+      expect(activityList.count()).toBe(3);
+
+      query.clear();
+      query.sendKeys('bird');
+      expect(activityList.count()).toBe(1);
+    });
   });
-
 });
