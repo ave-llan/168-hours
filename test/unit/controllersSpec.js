3fe3d851f0c678cdd19a98ddada237693344e58f
John Leszczynski <jrleszczynski@gmail.com>
2015-11-03 11:20:31 -0500

    add simple test

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,11 +1,18 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
+describe('oneSixtyEightApp controllers', function() {
 
-describe('controllers', function() {
+  describe('ScheduleListCtrl', function(){
 
-  it("should do something", function() {
+    beforeEach(module('oneSixtyEightApp'))
 
-  });
+    it('should create "activities" model with 16 activities', inject(function($controller) {
+      var scope = {},
+          ctrl = $controller('ScheduleListCtrl', {$scope:scope})
 
-});
+      expect(scope.activities.length).toBe(16)
+    }))
+
+  })
+})
