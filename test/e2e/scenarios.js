166c57e0fd61ac6cc1eda07be957e48d173723a3
John Leszczynski <jrleszczynski@gmail.com>
2015-11-04 11:08:37 -0500

    add tests for sorting

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,9 +2,9 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('168 Hours App', function() {
+describe('168 Hours App', function () {
 
-  describe('Activity list view', function() {
+  describe('Activity list view', function () {
 
     beforeEach(function() {
       browser.get('app/index.html')
@@ -13,7 +13,7 @@ describe('168 Hours App', function() {
     var activityList = element.all(by.repeater('activity in activities'))
     var query = element(by.model('query'))
 
-    it('should filter the activty list as a user types into the search box', function() {
+    it('should filter the activty list as a user types into the search box', function () {
 
       expect(activityList.count()).toBe(16)
 
@@ -23,6 +23,35 @@ describe('168 Hours App', function() {
       query.clear();
       query.sendKeys('bird');
       expect(activityList.count()).toBe(1)
+    })
+
+    it('should be possible to control activity order via the drop down select box', function () {
+
+      var activityNameColumn = element.all(by.repeater('activity in activities').column('activity.name'))
+      var query = element(by.model('query'))
+
+      function getNames () {
+        return activityNameColumn.map(function (elm) {
+          return elm.getText()
+        })
+      }
+
+      query.sendKeys('leisure') // narrow data set
+
+      expect(getNames()).toEqual([
+        'read novel',
+        'birdwatching',
+        'watch a movie'
+        ])
+
+      element(by.model('orderProp')).element(by.css('option[value="name"]')).click()
+
+      expect(getNames()).toEqual([
+        'birdwatching',
+        'read novel',
+        'watch a movie'
+        ])
+
     })
 
   })
