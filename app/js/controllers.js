4b11294f9388860024dc7343096a7c7a13342247
John Leszczynski <jrleszczynski@gmail.com>
2015-11-04 11:33:10 -0500

    declare services in array

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,26 +4,26 @@
 
 var oneSixtyEightApp = angular.module('oneSixtyEightApp', [])
 
-oneSixtyEightApp.controller('ScheduleListCtrl', function($scope, $http) {
-  $http.get('activities/activities.json').success(function (data) {
+oneSixtyEightApp.controller('ScheduleListCtrl', ['$scope', '$http',
+  function($scope, $http) {
+    $http.get('activities/activities.json').success(function (data) {
 
-    $scope.activities = data.map(function (activity) {
-      activity.hoursPerWeek = activity.minutes / 60 * activity.daysPerWeek
-      return activity
+      $scope.activities = data.map(function (activity) {
+        activity.hoursPerWeek = activity.minutes / 60 * activity.daysPerWeek
+        return activity
+      })
+
+      $scope.timeRemaining = calculateTimeRemaining()
     })
 
-    $scope.timeRemaining = calculateTimeRemaining()
-  })
+    $scope.orderProp = '-hoursPerWeek'
 
-  $scope.orderProp = '-hoursPerWeek'
-
-  function calculateTimeRemaining () {
-    var timeUsed = $scope.activities.map(function (activity) {
-      return activity.minutes * activity.daysPerWeek
-    }).reduce(function (sum, time) {
-      return sum + time
-    })
-    return 60 * 168 - timeUsed // in minutes
-  }
-
-})
+    function calculateTimeRemaining () {
+      var timeUsed = $scope.activities.map(function (activity) {
+        return activity.minutes * activity.daysPerWeek
+      }).reduce(function (sum, time) {
+        return sum + time
+      })
+      return 60 * 168 - timeUsed // in minutes
+    }
+}])
