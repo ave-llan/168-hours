a9f3bd7d843e167057b2e369a445318ed4475f7c
John Leszczynski <jrleszczynski@gmail.com>
2015-11-04 10:58:53 -0500

    revert to former test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -7,23 +7,23 @@ describe('168 Hours App', function() {
   describe('Activity list view', function() {
 
     beforeEach(function() {
-      browser.get('app/index.html');
+      browser.get('app/index.html')
     });
 
+    var activityList = element.all(by.repeater('activity in activities'))
+    var query = element(by.model('query'))
 
     it('should filter the activty list as a user types into the search box', function() {
 
-      var activityList = element.all(by.repeater('activity in activities'));
-      var query = element(by.model('query'));
-
-      expect(activityList.count()).toBe(16);
+      expect(activityList.count()).toBe(16)
 
       query.sendKeys('rudy');
-      expect(activityList.count()).toBe(3);
+      expect(activityList.count()).toBe(3)
 
       query.clear();
       query.sendKeys('bird');
-      expect(activityList.count()).toBe(1);
-    });
-  });
-});
+      expect(activityList.count()).toBe(1)
+    })
+
+  })
+})
