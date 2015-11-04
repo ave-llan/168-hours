df5f816959157a9db7f2dd95969788ec57319cd1
John Leszczynski <jrleszczynski@gmail.com>
2015-11-04 11:49:43 -0500

    use httpBackend for tests

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -4,20 +4,48 @@
 describe('oneSixtyEightApp controllers', function() {
 
   describe('ScheduleListCtrl', function(){
-    var scope, ctrl
+    var scope, ctrl, $httpBackend
 
     beforeEach(module('oneSixtyEightApp'))
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_
+      $httpBackend.expectGET('activities/activities.json').
+        respond([
+          {
+            "name": "sleep",
+            "minutes": 420,
+            "daysPerWeek": 7,
+            "category": "sleep"
+          },
+          {
+            "name": "eat breakfast",
+            "minutes": 45,
+            "daysPerWeek": 7,
+            "category": "food"
+          },
+          {
+            "name": "Rudy AM walk",
+            "minutes": 30,
+            "daysPerWeek": 7,
+            "category": "pet"
+          }
+        ])
 
-    beforeEach(inject(function($controller) {
-      scope = {}
-      ctrl = $controller('ScheduleListCtrl', {$scope:scope})
+      scope = $rootScope.$new()
+      ctrl = $controller('ScheduleListCtrl', { $scope:scope })
     }))
 
-    it('should create "activities" model with 16 activities', function() {
-      expect(scope.activities.length).toBe(16)
+    it('should create "activities" model with 3 activities', function() {
+      expect(scope.activities).toBeUndefined()
+      $httpBackend.flush()
+
+      expect(scope.activities.length).toBe(3)
+      expect(scope.activities.map(function (activity) { return activity.category })).toEqual([
+        'sleep', 'food', 'pet'])
     })
 
     it('should set the default value of orderProp model', function() {
+      $httpBackend.flush()
       expect(scope.orderProp).toBe('-hoursPerWeek')
     })
 
