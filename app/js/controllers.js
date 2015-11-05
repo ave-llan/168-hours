16ff01b578c999dde488ff0d4b3f48d62c7416db
John Leszczynski <jrleszczynski@gmail.com>
2015-11-05 11:00:06 -0500

    update remaining hours when changes detected

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,7 +8,10 @@ oneSixtyEightApp.controller('ScheduleListCtrl', ['$scope', '$http',
   function($scope, $http) {
     $http.get('activities/activities.json').success(function (data) {
 
-      $scope.activities = data
+      $scope.activities = data.map(function (activity) {
+        activity.hoursPerWeek = (activity.minutes / 60 + activity.hours) * activity.daysPerWeek
+        return activity
+      })
 
       $scope.timeRemaining = calculateTimeRemaining()
     })
@@ -22,5 +25,18 @@ oneSixtyEightApp.controller('ScheduleListCtrl', ['$scope', '$http',
         return sum + time
       })
       return 60 * 168 - timeUsed // in minutes
+    }
+
+    // returns and sets the hours per week for this activity
+    $scope.hoursPerWeek = function (activityName) {
+      var activity = $scope.activities.filter(function (activity) {
+        return activity.name === activityName
+      })[0] // take the first item (TODO add unique ID so there can be multipe items with same name)
+      activity.hoursPerWeek = (activity.minutes/60 + activity.hours) * activity.daysPerWeek
+      return activity.hoursPerWeek
+    }
+
+    $scope.updateHoursLeft = function () {
+      $scope.timeRemaining = calculateTimeRemaining()
     }
 }])
