5f2cf2643b9d2ea3a2cc289a79d5ef5864414483
John Leszczynski <jrleszczynski@gmail.com>
2015-11-18 06:11:24 -0500

    add gulp task to map scss to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp')
+var sass = require('gulp-sass')
 
-gulp.task('default', function() {
+gulp.task('default', function () {
   // place code for your default task here
+})
+
+gulp.task('styles', function () {
+  gulp.src('./app/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/css'))
 })
