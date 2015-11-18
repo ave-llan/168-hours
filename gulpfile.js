fb5e2ab25aa792b45a716b84ed56da3e9cb3a9c5
John Leszczynski <jrleszczynski@gmail.com>
2015-11-18 10:00:09 -0500

    configure gulp serve

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,30 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 var autoprefixer = require('gulp-autoprefixer')
+var browserSync = require('browser-sync').create()
 
-/**
- * @param {String[]} - names of tasks that will be run
- * @returns {Function} - returns a function that takes the gulp event object and logs info
- **/
-var watchLogger = function (taskNames) {
-  return function (event) {
-    console.log('File', event.path, 'was', event.type,
-                ', running tasks:', taskNames.join(', ') + '...')
-  }
-}
+var reload = browserSync.reload
+
+// static server + watching scss/html files
+gulp.task('serve', ['styles'], function () {
+
+  browserSync.init({
+    server: './app',
+    port: 3000
+  })
+
+  gulp.watch(['app/**/*.html'], reload)
+  gulp.watch(['app/**/*.{scss,css}'], ['styles', reload])
+  gulp.watch(['app/**/*.js'], reload)
+
+})
 
 gulp.task('default', function () {
   gulp.watch('./app/sass/**/*.scss', ['styles'])
-    .on('change', watchLogger(['styles', 'frying', 'baking']))
+    .on('change', watchLogger(['styles']))
 })
 
+// sass => css with autoprefixes
 gulp.task('styles', function () {
   gulp.src('./app/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -26,3 +33,15 @@ gulp.task('styles', function () {
     }))
     .pipe(gulp.dest('./app/css'))
 })
+
+
+/**
+ * @param {String[]} - names of tasks that will be run
+ * @returns {Function} - returns a function that takes the gulp event object and logs info
+ */
+function watchLogger (taskNames) {
+  return function (event) {
+    console.log('File', event.path, 'was', event.type,
+                ', running tasks:', taskNames.join(', ') + '...')
+  }
+}
