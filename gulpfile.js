a7ad125f51ade0ac3b40a7bd55685062d12e3e6b
John Leszczynski <jrleszczynski@gmail.com>
2015-11-18 07:01:40 -0500

    create watch logger to log info about what happened when watched files change

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,20 @@ var gulp = require('gulp')
 var sass = require('gulp-sass')
 var autoprefixer = require('gulp-autoprefixer')
 
+/**
+ * @param {String[]} - names of tasks that will be run
+ * @returns {Function} - returns a function that takes the gulp event object and logs info
+ **/
+var watchLogger = function (taskNames) {
+  return function (event) {
+    console.log('File', event.path, 'was', event.type,
+                ', running tasks:', taskNames.join(', ') + '...')
+  }
+}
+
 gulp.task('default', function () {
-  // place code for your default task here
+  gulp.watch('./app/sass/**/*.scss', ['styles'])
+    .on('change', watchLogger(['styles', 'frying', 'baking']))
 })
 
 gulp.task('styles', function () {
