6581132f957c8157c7d33792cd159ce4cb66b06f
John Leszczynski <jrleszczynski@gmail.com>
2015-11-19 10:42:42 -0500

    move server into default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,10 @@ var gulp         = require('gulp')
   , autoprefixer = require('gulp-autoprefixer')
   , eslint       = require('gulp-eslint')
   , browserSync  = require('browser-sync').create()
+  , reload       = browserSync.reload
 
-var reload = browserSync.reload
-
-gulp.task('lint', function () {
-  // ESLint ignores files with "node_modules" paths.
-  // So, it's best to have gulp ignore the directory as well.
-  // Also, Be sure to return the stream from the task;
-  // Otherwise, the task may end before the stream has finished.
-  return gulp.src(['**/*.js','!node_modules/**','!app/bower_components/**'])
-    // eslint() attaches the lint output to the "eslint" property
-    // of the file object so it can be used by other modules.
-    .pipe(eslint())
-    // eslint.format() outputs the lint results to the console.
-    // Alternatively use eslint.formatEach() (see Docs).
-    .pipe(eslint.format())
-    // To have the process exit with an error code (1) on
-    // lint error, return the stream and pipe to failAfterError last.
-    .pipe(eslint.failAfterError())
-})
-
-// static server + watching scss/html files
-gulp.task('serve', ['styles'], function () {
+// static server + linting and recompiling and reloading after any changes
+gulp.task('default', ['styles', 'lint'], function () {
 
   browserSync.init({
     server: './app',
@@ -33,14 +15,19 @@ gulp.task('serve', ['styles'], function () {
 
   gulp.watch(['app/**/*.html'], reload)
   gulp.watch(['app/**/*.{scss,css}'], ['styles', reload])
-  gulp.watch(['app/**/*.js'], reload)
-
-})
-
-gulp.task('default', function () {
-  gulp.watch('./app/sass/**/*.scss', ['styles'])
     .on('change', watchLogger(['styles']))
+  gulp.watch(['app/**/*.js'], ['lint', reload])
+    .on('change', watchLogger(['lint']))
 })
+
+// lint all .js files in the project
+gulp.task('lint', function () {
+  return gulp.src(['**/*.js','!node_modules/**','!app/bower_components/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+})
+
 
 // sass => css with autoprefixes
 gulp.task('styles', function () {
