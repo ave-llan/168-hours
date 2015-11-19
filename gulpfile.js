38c0de7ec3bf1dcbeb7ab28b0012b6e3c9abd0b3
John Leszczynski <jrleszczynski@gmail.com>
2015-11-19 10:01:21 -0500

    install eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,27 @@
 var gulp         = require('gulp')
   , sass         = require('gulp-sass')
   , autoprefixer = require('gulp-autoprefixer')
+  , eslint       = require('gulp-eslint')
   , browserSync  = require('browser-sync').create()
 
 var reload = browserSync.reload
+
+gulp.task('lint', function () {
+  // ESLint ignores files with "node_modules" paths.
+  // So, it's best to have gulp ignore the directory as well.
+  // Also, Be sure to return the stream from the task;
+  // Otherwise, the task may end before the stream has finished.
+  return gulp.src(['**/*.js','!node_modules/**','!app/bower_components/**'])
+    // eslint() attaches the lint output to the "eslint" property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError())
+})
 
 // static server + watching scss/html files
 gulp.task('serve', ['styles'], function () {
